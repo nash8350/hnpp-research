07d7d8917a0877f92bd542bc6bf148ef75d60ea9
nash8350 <nash8350@users.noreply.github.com>
2018-03-23 09:56:58 -0700

    fixing about

--- src/pages/about/about.md
+++ src/pages/about/about.md
@@ -2,6 +2,6 @@
 templateKey: 'about-page'
 path: /about
 title: About
-description: >-
-  Hereditary Neuropathy with Liability to Pressure Palsies (HNPP) is a genetic disease affecting less than 1 in 5,000 people.  
 ---
+
+Hereditary Neuropathy with Liability to Pressure Palsies (HNPP) is a genetic disease affecting less than 1 in 5,000 people.  
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import graphql from 'graphql';
+import Content, { HTMLContent } from '../components/Content';
 
-export const AboutPageTemplate = ({
-  title, description, data}) => (
-  <section className="section section--gradient">
-    <div className="container">
+export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content;
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
       <h3 className="has-text-weight-semibold is-size-4">{title}</h3>
-      <p>{description}</p>
-    </div>
-  </section>
-);
+        <PageContent className="content" content={content} />
+      </div>
+    </section>
+  );
+};
 
 export default ({ data }) => {
-  return (
-    <AboutPageTemplate
-      title={data.page.frontmatter.title}
-      description={data.page.frontmatter.description}
-    />
-  );
+  const { markdownRemark: post } = data;
+
+  return (<AboutPageTemplate
+    contentComponent={HTMLContent}
+    title={post.frontmatter.title}
+    content={post.html}
+  />);
 };
 
 export const aboutPageQuery = graphql`
   query AboutPage($path: String!) {
-    page: markdownRemark(frontmatter: {path: {eq: $path}}) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
       frontmatter {
+        path
         title
-        description
       }
     }
-  }  
+  }
 `;
